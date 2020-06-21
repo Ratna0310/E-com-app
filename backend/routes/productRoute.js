052f440d4eb6f8343687bb1ad3f0794cacb3dce4
Horizon03 <ratnadobhal@gmail.com>
2020-06-21 10:03:56 +0530

    added search functionality

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -2,28 +2,15 @@ import express from 'express';
 import data from '../data.js';
 const router = express.Router();
 
-// router.get("/", async (req, res) => {
-//   const category = req.query.category ? { category: req.query.category } : {};
-//   const searchKeyword = req.query.searchKeyword ? {
-//     name: {
-//       $regex: req.query.searchKeyword,
-//       $options: 'i'
-//     }
-//   } : {};
-//   const sortOrder = req.query.sortOrder ?
-//     (req.query.sortOrder === 'lowest' ? { price: 1 } : { price: -1 })
-//     :
-//     { _id: -1 };
-//     //const products =  data.products.find({ ...category, ...searchKeyword }).sort(sortOrder);
-//   const product = data.products.filter(item =>
-//     item.name == req.query.searchKeyword
+ router.get("/", async (req, res) => {
+   const searchKeyWord = new RegExp(req.query.search, 'g');
+  const products =  data.products.filter(item => item.name.match(searchKeyWord));
 
-//   );
-//   if(product)
-//   res.send(product);
-//   else
-//   res.send(data.products);
-// });
+  if(req.query.search)
+    res.send(products);
+  else 
+    res.send(data.products);
+ });
 
 router.get("/:id", async (req, res) => {
   const productId = req.params.id;
@@ -34,8 +21,8 @@ router.get("/:id", async (req, res) => {
     res.status(404).send({ msg: "Product Not Found." })
 });
 
-router.get("/", (req, res) => {
-  res.send(data.products);
-});
+// router.get("/", (req, res) => {
+//   res.send(data.products);
+// });
 
 export default router;
